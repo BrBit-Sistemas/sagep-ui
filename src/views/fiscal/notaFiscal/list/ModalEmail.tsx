4aa3efed93bbb51fb06a949d4beb672e9974eec9
maTorresani <maira.torresani@gmail.com>
2023-03-23 16:01:47 -0300

    formulario pronto no modal de email on issue #31

--- src/views/fiscal/notaFiscal/list/ModalEmail.tsx
+++ src/views/fiscal/notaFiscal/list/ModalEmail.tsx
@@ -7,15 +7,21 @@ import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import IconButton from '@mui/material/IconButton';
 import { Close } from 'mdi-material-ui';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 // ** Import Toast
 import { toast } from "react-toastify"; 
 import 'react-toastify/dist/ReactToastify.css';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
 
-import { Button } from '@mui/material';
+// ** form Imports
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm, Controller } from 'react-hook-form'
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
 
 interface ModalEmailProps {
     openModalEmail: boolean
@@ -23,17 +29,35 @@ interface ModalEmailProps {
     selectedId: string
 }
 
+const schema = yup.object().shape({
+    email: yup
+      .string()
+      .required("E-mail é obrigatório.")
+  })
+
 const ModalEmail = (props: ModalEmailProps) => {
 
-    function sendEmail() {
+    // ** Props
+    const { openModalEmail, setOpenModalEmail, selectedId } = props
+
+    const {
+      reset,
+      control,
+      setValue,
+      handleSubmit,
+      formState: { errors }
+    } = useForm({
+      mode: 'onChange',
+      resolver: yupResolver(schema)
+    })
+    const onSubmit = (data: {email: string}) => {
+        console.log(data)
+        reset()
         console.log("Enviar email nota id: "+selectedId);
         //enviar para o backend
         setOpenModalEmail(false);
         toast.success("Enviada com sucesso!");
     }
-
-    // ** Props
-    const { openModalEmail, setOpenModalEmail, selectedId } = props
 
     return (
 
@@ -57,17 +81,30 @@ const ModalEmail = (props: ModalEmailProps) => {
                         Enviar nota por e-mail
                     </Typography>
                 </Box>
-                <form onSubmit={(event) => { event.preventDefault(); sendEmail() }}>
-                    <Grid container spacing={2} justifyContent="center">
-                        <Grid item xs={12} sx={{ mb: 2 }}>
-                            <TextField label="E-mail" variant="outlined" autoFocus required placeholder='exemplo@email.com' fullWidth type="email" InputLabelProps={{ shrink: true }} />
-                        </Grid>
-                        <Grid item xs="auto">
-                            <Button variant='contained' type="submit">
-                                Enviar
-                            </Button>
-                        </Grid>
-                    </Grid>
+                
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormControl fullWidth sx={{ mb: 4 }}>
+                        <Controller
+                            name='email'
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field: { value, onChange } }) => (
+                                <TextField
+                                    value={value}
+                                    onChange={onChange}
+                                    placeholder='exemplo@email.com'
+                                    error={Boolean(errors.email)}
+                                    label='E-mail'
+                                />
+                            )}
+                        />
+                        {errors.email && <FormHelperText sx={{ color: 'error.main' }}>{errors.email.message}</FormHelperText>}
+                    </FormControl>
+                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button size='large' type='submit' variant='contained'>
+                            Enviar
+                        </Button>
+                    </Box>
                 </form>
             </DialogContent>
         </Dialog>
